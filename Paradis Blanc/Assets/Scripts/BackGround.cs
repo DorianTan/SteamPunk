2e327b440f7646dd837e40ec5729a39fbef465a1
Tan <doriantan7@gmail.com>
2019-02-20 15:00:57 +0100

    background spawn

---
+++ Paradis Blanc/Assets/Scripts/BackGround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x-(MapManager.Instance.Speed*Time.deltaTime), transform.position.y, transform.position.z); // deplacement du background
+
+        if (transform.position.x <= -40)
+        {
+            Destroy(gameObject);// destruction sortie de l'ecran
+        }
+    }
+}
---
+++ Paradis Blanc/Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject [] Obstacles; // prefab des enemies
+
+    [SerializeField] private float nextActionTime = 5f; // temps initial entre chaque spawn
+    [SerializeField] private float period = 0.1f; // augmentation du temps entre chaque spawn
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        if (Time.timeSinceLevelLoad > nextActionTime)
+	    {
+	        nextActionTime += period;
+	        Instantiate(Obstacles[Random.Range(0, Obstacles.Length)], new Vector3(25, Random.Range(-4, 4), 1), new Quaternion(0, 0, 0, 0)); //Vector 3 = position aléatoire sur l'axe y
+	        if (period > 1)
+	        {
+	            period /= 1.5f;
+	        }
+	        else
+	        {
+	            period -= 0.01f;
+	        }
+	    }
+	   
+    }
+
+}
